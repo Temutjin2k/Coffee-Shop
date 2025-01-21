d749a73279a69aa9121ce8803394a866a81ed19d
tkoszhan <tamutdzhin2006@mail.ru>
2025-01-21 16:56:58 +0500

    Postman collection for testing

--- cmd/main.go
+++ cmd/main.go
@@ -28,7 +28,7 @@ func main() {
 	err = db.Ping()
 	// Если ошибка подключения, пробуем несколько раз
 	if err != nil {
-		logger.Error("Error connecting to database: %v", err)
+		logger.Error(fmt.Sprintf("Error connecting to database: %v", err))
 
 		// Пробуем подключиться до 5 раз с интервалом в 5 секунд
 		for i := 0; i < 5; i++ {
